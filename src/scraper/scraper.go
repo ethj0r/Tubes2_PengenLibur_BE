e3c054d7f3b857f2faccec4c62c01eda0a3291a3
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2026-04-12 15:09:35 +0700

    feat(scraper): scraper

--- src/scraper/scraper.go
+++ src/scraper/scraper.go
@@ -1,1 +1,20 @@
 package scraper
+
+import (
+	"io"
+	"net/http"
+)
+
+// return html hasil scraping
+func Scraper(url string) (string, int, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		// handle error
+		return "", response.StatusCode, err
+	}
+
+	defer response.Body.Close()
+
+	bytes, _ := io.ReadAll(response.Body)
+	return string(bytes), response.StatusCode, nil
+}
