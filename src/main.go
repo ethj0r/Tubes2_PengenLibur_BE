07139e0f7f789aa3b72a65a3e6fd2ba5b89d8b00
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2026-04-15 23:05:25 +0700

    feat(main) : to help simple testing

---
+++ src/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"backend/src/parser"
+	"backend/src/scraper"
+	"fmt"
+)
+
+func main() {
+	src, _, _ := scraper.Scraper("https://www.Example.com/")
+	lagiTesting := false
+
+	if src != nil && lagiTesting {
+		fmt.Printf("Success!\n")
+		fmt.Print(string(src))
+		return
+	}
+	root, err := parser.Parse(src)
+	if err != nil {
+		fmt.Print("Error!\n")
+	}
+	root.PrintTree(0) // start depth :/
+}
+
+/*
+Example.com
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Example Domain</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <style>
+            body {
+                background: #eee;
+                width: 60vw;
+                margin: 15vh auto;
+                font-family: system-ui, sans-serif;
+            }
+
+            h1 {
+                font-size: 1.5em;
+            }
+
+            div {
+                opacity: 0.8;
+            }
+
+            a:link,
+            a:visited {
+                color: #348;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div>
+            <h1>Example Domain</h1>
+
+            <p>
+                This domain is for use in documentation examples without needing permission.
+                Avoid use in operations.
+            </p>
+
+            <p>
+                <a href="https://iana.org/domains/example">Learn more</a>
+            </p>
+        </div>
+    </body>
+</html>
+*/
