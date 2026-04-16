194e0483b4a771808f4583b20c509222007e21bb
Daniel <19624107@mahasiswa.itb.ac.id>
2026-04-16 22:38:46 +0700

    feat: add checker for combinator selectors

--- src/algorithm/helper.go
+++ src/algorithm/helper.go
@@ -71,3 +71,66 @@ func (node *Node) includeNode(selector string) bool {
 func (node *Node) hasChildren() bool {
 	return node != nil && len(node.Children) > 0
 }
+
+// mengecek apakah node adalah child langsung dari parent (parent > child)
+func (node *Node) isDirectChildOf(parent *Node) bool {
+	return node != nil && parent != nil && node.Parent == parent
+}
+
+// mengecek apakah node turunan dari ancestor (ancestor descendant)
+func (node *Node) isDescendantOf(ancestor *Node) bool {
+	if node == nil || ancestor == nil {
+		return false
+	}
+
+	// telusuri hingga atas
+	for cur := node.Parent; cur != nil; cur = cur.Parent {
+		if cur == ancestor {
+			return true
+		}
+	}
+
+	return false
+}
+
+// mengecek apakah node sibling tepat setelah sibling lain (sibling1 + sibling2)
+func (node *Node) isAdjacentSiblingOf(sibling *Node) bool {
+	if node == nil || sibling == nil || node.Parent == nil || node.Parent != sibling.Parent {
+		return false
+	}
+
+	siblings := node.Parent.Children
+	// mengecek bersebelahan
+	for i := 1; i < len(siblings); i++ {
+		if siblings[i] == node && siblings[i-1] == sibling {
+			return true
+		}
+	}
+
+	return false
+}
+
+// mengecek apakah node adalah sibling setelah sibling lain (sibling1 ~ sibling2)
+func (node *Node) isGeneralSiblingOf(sibling *Node) bool {
+	if node == nil || sibling == nil || node.Parent == nil || node.Parent != sibling.Parent {
+		return false
+	}
+
+	siblings := node.Parent.Children
+	idxNode := -1
+	idxSibling := -1
+
+	// traversal semuanya
+	for i, s := range siblings {
+		if s == node {
+			idxNode = i
+		}
+
+		if s == sibling {
+			idxSibling = i
+		}
+	}
+
+	// sibling harus sebelum node 
+	return idxNode > idxSibling && idxSibling >= 0
+}
