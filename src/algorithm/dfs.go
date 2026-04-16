82e58634736d78a6b460db966b61c191cf1cdb84
Daniel <19624107@mahasiswa.itb.ac.id>
2026-04-17 06:47:49 +0700

    feat: add search by dfs implementation

--- src/algorithm/dfs.go
+++ src/algorithm/dfs.go
@@ -1,1 +1,28 @@
 package algorithm
+
+// implement DFS secara rekursif
+func (node *Node) dfsSearch(selector string) []*Node {
+	var result []*Node
+
+	// empty node
+	if node == nil {
+		return result
+	}
+
+	// insert node ke hasil sesuai filter
+	if node.includeNode(selector) {
+		result = append(result, node)
+	}
+
+	if node.hasChildren() {
+		// explore hingga paling dalam, baru pindah ke cabang selanjutnya
+		for _, child := range node.Children {
+			if child != nil {
+				childResult := child.dfsSearch(selector)
+				result = append(result, childResult...)
+			}
+		}
+	}
+
+	return result
+}
