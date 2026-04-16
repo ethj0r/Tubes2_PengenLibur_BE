991d7f7acfcd9363283a7c389fc2b6c459e7c6c8
Daniel <19624107@mahasiswa.itb.ac.id>
2026-04-16 22:02:46 +0700

    feat: add search by bfs implementation

--- src/algorithm/bfs.go
+++ src/algorithm/bfs.go
@@ -1,1 +1,39 @@
 package algorithm
+
+// implement BFS pake queue
+func (node *Node) bfsSearch(selector string) []*Node {
+	var result []*Node
+
+	// node kosong
+	if node == nil {
+		return result
+	}
+
+	queue := []*Node{node} // antrian node
+
+	for len(queue) > 0 {
+		// ambil node paling depan & hapus dari queue
+		curr := queue[0]
+		queue = queue[1:]
+
+		if curr == nil { 
+			continue
+		}
+
+		// insert node ke hasil sesuai filter
+		if curr.includeNode(selector) {
+			result = append(result, curr)
+		}
+
+		if curr.hasChildren() {
+			// insert child ke belakang queue (proses level by level)
+			for _, child := range curr.Children {
+				if child != nil {
+					queue = append(queue, child)
+				}
+			}
+		}
+	}
+
+	return result
+}
