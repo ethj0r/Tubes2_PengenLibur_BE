8ae1b7a17916d547a80d5c5cad391834b623efc9
Daniel <19624107@mahasiswa.itb.ac.id>
2026-04-16 20:51:00 +0700

    feat: add cases for isMacth func to support multi class

--- src/algorithm/helper.go
+++ src/algorithm/helper.go
@@ -1,20 +1,37 @@
-package algorithm
+﻿package algorithm
 
-func (node* Node) isMatch(selector string) bool {
-	// mengecek tag name / class
-	if node.Elmt.Data == selector {
-		return true
+import "strings"
+
+// mengecek apakah node sesuai dengan selector yang diinginkan
+func (node *Node) isMatch(selector string) bool {
+	// node / selector kosong
+	if node == nil || selector == "" {
+		return false
 	}
 
+	// tag selector (no prefix)
+	if selector[0] != '.' && selector[0] != '#' {
+		return node.Elmt.Data == selector
+	}
+
+	// class dan id selector
 	for _, atr := range node.Elmt.Attributes {
-		// mengecek class selector
-		if atr.Name == "class" && "."+atr.Value == selector {
-			return true
+		// class selector (bisa ada spasi, misal "p1 p2 p3")
+		if atr.Name == "class" && selector[0] == '.' {
+			classes := strings.Split(atr.Value, " ") // split spasi
+
+			for _, cls := range classes {
+				if "." + strings.TrimSpace(cls) == selector { // remove spasi yang masuk
+					return true
+				}
+			}
 		}
 
-		// mengecek id selector
-		if atr.Name == "id" && "#"+atr.Value == selector {
-			return true
+		// id selector
+		if atr.Name == "id" && selector[0] == '#' {
+			if "#" + strings.TrimSpace(atr.Value) == selector {
+				return true
+			}
 		}
 	}
 
